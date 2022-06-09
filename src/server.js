83c446eaaace004d7e9fce30f67f3ba0d06ebade
WKlee0607 <fovert@khu.ac.kr>
2022-06-10 01:28:16 +0900

    req.body를 ES6가 이해하도록.
    form에서 POST해줄 때, form 내의 입력 내용을 받아오기 위해 req.body를 사용해야 되는데, req.body를 ES6가 이해하도록 설정해주는 과정

--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 import express from "express";// 혹은 "node_modules/express"
+import { urlencoded } from "express";
 import morgan from "morgan";
 import globalRouter from "./routers/globalRouter";
 import userRouter from "./routers/userRouter";
@@ -12,8 +13,9 @@ const app = express();
 const logger = morgan("dev");
 
 app.set("view engine","pug");
-app.set("views",process.cwd() + "/src/views");
+app.set("views",process.cwd() + "/src/views");//process.cwd: 현재 작업중인 파일 위치.
 app.use(logger);
+app.use(express.urlencoded({extende:true}));//form의 내용을 js의 array object 형태로 바꿔줌-> req.body를 생성함!
 app.use("/",globalRouter);
 app.use("/videos",videoRouter);
 app.use("/users",userRouter);
