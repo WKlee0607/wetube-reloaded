1210e0fcebaa9f41327ef59fce4bcc361f35a48a
WKlee0607 <fovert@khu.ac.kr>
2022-06-13 18:21:52 +0900

    server외의 값은 init.js로 이동

--- src/server.js
+++ src/server.js
@@ -5,10 +5,6 @@ import globalRouter from "./routers/globalRouter";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
 
-const PORT = 4000;
-
-
-
 const app = express(); 
 const logger = morgan("dev");
 
@@ -21,8 +17,4 @@ app.use("/videos",videoRouter);
 app.use("/users",userRouter);
 
 
-const handleListening = () => 
-    console.log(`Server listening on port http://localhost:${PORT}`);
-
-
-app.listen(PORT, handleListening);
+export default app;
