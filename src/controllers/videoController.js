4f2da6ad990921f2ea7ec12fe838c4cdc767d13d
WKlee0607 <fovert@khu.ac.kr>
2022-06-13 18:25:50 +0900

    postUpload
    가짜 데이터베이스 삭제 및 postUpload 생성.
    postUpload의 new Object(model로 찍어내기) 생성 및 real DB(mongoDB)에 저장하는 코드 적기, 오류 처리하는 코드 적기

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,48 +1,44 @@
-let videos = [
-    {
-        title:"First Video",
-        rating:5,
-        comment:2,
-        createdAt:"2 minutes ago",
-        views:59,
-        id:1,
-    },
-    {
-         title:"Second Video",
-         rating:5,
-         comment:2,
-         createdAt:"2 minutes ago",
-         views:59,
-         id:2,
-     },
-     {
-         title:"Third Video",
-         rating:5,
-         comment:2,
-         createdAt:"2 minutes ago",
-         views:59,
-         id:3,
-     },
-];
-export const trending = (req,res) => {
-    return res.render("home",{pageTitle : "Home", videos});
-}
+import Video from "../models/Video";
+
+export const home = async(req,res) => {
+        const videos = await Video.find({});//await: 해당 코드가 끝날 때까지 다음 순서의 코드를 진행시키지 않음. 즉, 해당 코드를 기다려주는 역할을 함.
+        console.log(videos);
+        return res.render("home",{pageTitle : "Home",videos});//videos를 db에서 받아옴.
+};
+
 export const watch = (req,res) => {
     const id = req.params.id;
-    const video = videos[id-1];
-    return res.render("watch",{pageTitle : `Watching: ${video.title}`,video});
+    return res.render("watch",{pageTitle : `Watching`});
 }
 export const getEdit = (req,res) => {
     const id = req.params.id;
-    const video = videos[id-1];
-    return res.render("edit",{pageTitle:`Editing: ${video.title}`,video});
+    return res.render("edit",{pageTitle:`Editing`});
 }
 export const postEdit = (req, res) => {
     const {id} = req.params;
     const {title} = req.body;
-    videos[id-1].title = title;
     return res.redirect(`/videos/${id}`);
 }
+export const getUpload = (req,res) => {
+    return res.render("upload",{pageTitle:"Upload Video"});
+}
 
-
+export const postUpload = async(req,res) => {
+    // here we will add a video to the videos array.
+    const { title, description, hashtags } = req.body;
+    try{
+        await Video.create({
+            title:title,
+            description:description,
+            hashtags: hashtags.split(",").map((word) => `#${word}`),
+        });
+        return res.redirect("/");
+    }
+    catch(error){
+        return res.render("upload",{
+            pageTitle:"Upload Video",
+            errorMessage: error._message,
+        });
+    }
+}
 
