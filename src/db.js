4ac1d6a859ee9d47e77363865f71abb7f12701e3
WKlee0607 <fovert@khu.ac.kr>
2022-06-13 18:21:30 +0900

    db파일 생성 및 db파일 connection

---
+++ src/db.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+mongoose.connect("mongodb://127.0.0.1:27017/wetube", {//mongoDB의 url서버에 연결해준 것임.
+    useNewUrlParser:true,
+    useUnifiedTopology:true,
+});
+
+const db = mongoose.connection;//mongoose가 mongoDB에 대한 connection 접근을 줌.
+//그래서 우리는 서버와 database서버의 사이의 현재 connection에 액세스 할 수 있음.
+const handleOpne = () => console.log("✅ Connected to DB");
+const handleError = (error) => console.log("❌ DB Error", error);
+db.on("error",handleError);
+db.once("open",handleOpne);
