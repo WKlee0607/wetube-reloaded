aede4ba5aa1f0a08d3e95f3dae339e9516b4a198
WKlee0607 <fovert@khu.ac.kr>
2022-06-14 00:14:55 +0900

    max & minlength update

--- src/models/Video.js
+++ src/models/Video.js
@@ -1,10 +1,10 @@
 import mongoose from "mongoose";
 
 const videoSchema = new mongoose.Schema({
-    title: {type:String, required:true},//{type:String}이라 써도됨.
-    description: {type:String, required:true},
+    title: {type:String, required:true, trim:true, maxlength:80},//{type:String}이라 써도됨.
+    description: {type:String, required:true, trim:true, minlength:10},
     createdAt: {type:Date, required:true, default:Date.now },
-    hashtags:[{type:String}],
+    hashtags:[{type:String, trim:true}],
     meta: {
         views:{type:Number, default:0, required:true},
         rating:{type:Number, default:0, required:true},
