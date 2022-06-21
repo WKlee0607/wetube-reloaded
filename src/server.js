e27f9237bb72ebcf191e80d6c90be5714fd46c40
WKlee0607 <fovert@khu.ac.kr>
2022-06-21 18:55:54 +0900

    오류 수정

--- src/server.js
+++ src/server.js
@@ -11,7 +11,7 @@ const logger = morgan("dev");
 app.set("view engine","pug");
 app.set("views",process.cwd() + "/src/views");//process.cwd: 현재 작업중인 파일 위치.
 app.use(logger);
-app.use(express.urlencoded({extende:true}));//form의 내용을 js의 array object 형태로 바꿔줌-> req.body를 생성함!
+app.use(express.urlencoded({extended:true}));//form의 내용을 js의 array object 형태로 바꿔줌-> req.body를 생성함!
 app.use("/",globalRouter);
 app.use("/videos",videoRouter);
 app.use("/users",userRouter);
