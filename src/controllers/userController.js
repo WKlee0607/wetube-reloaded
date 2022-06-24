c546bf2683f73bf6f2d0aa93bcc9795292ce2c11
WKlee0607 <fovert@khu.ac.kr>
2022-06-24 13:09:16 +0900

    postJoin 컨트롤러 업데이트
    mongo DB에 post해준 user값을 저장해줌. create() : mongoDB 함수.

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,4 +1,18 @@
-export const join = (req, res) => res.send("join");
+import User from "../models/User";
+
+export const getJoin = (req, res) => res.render("join",{pageTitle:"Join"});
+export const postJoin = async(req, res) => {
+    //console.log(req.body);
+    const { name, username, email, password, location } = req.body;
+    await User.create({
+        name, 
+        username, 
+        email, 
+        password, 
+        location,
+    })//User 생성
+    return res.redirect("/login");//회원가입하고 login창으로 보내주기
+};
 export const edit = (req, res) => res.send("Edit");
 export const remove = (req, res) => res.send("delete");
 export const login = (req,res) => res.send("Log in");
