0e50b5a1290b33d8c94b44f4da6891aa68079aac
WKlee0607 <fovert@khu.ac.kr>
2022-06-24 13:11:45 +0900

    global -> root

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 import express from "express";// 혹은 "node_modules/express"
 import { urlencoded } from "express";
 import morgan from "morgan";
-import globalRouter from "./routers/globalRouter";
+import rootRouter from "./routers/rootRouter";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
 
@@ -12,7 +12,7 @@ app.set("view engine","pug");
 app.set("views",process.cwd() + "/src/views");//process.cwd: 현재 작업중인 파일 위치.
 app.use(logger);
 app.use(express.urlencoded({extended:true}));//form의 내용을 js의 array object 형태로 바꿔줌-> req.body를 생성함!
-app.use("/",globalRouter);
+app.use("/",rootRouter);
 app.use("/videos",videoRouter);
 app.use("/users",userRouter);
 
