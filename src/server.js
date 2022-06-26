ba5d7cd9f649be5b0471d13d100aa0d26d080e9f
WKlee0607 <fovert@khu.ac.kr>
2022-06-26 18:08:29 +0900

    Session middleware

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import express from "express";// 혹은 "node_modules/express"
 import { urlencoded } from "express";
 import morgan from "morgan";
+import session from "express-session";
 import rootRouter from "./routers/rootRouter";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
@@ -12,6 +13,26 @@ app.set("view engine","pug");
 app.set("views",process.cwd() + "/src/views");//process.cwd: 현재 작업중인 파일 위치.
 app.use(logger);
 app.use(express.urlencoded({extended:true}));//form의 내용을 js의 array object 형태로 바꿔줌-> req.body를 생성함!
+
+
+app.use(
+    session({
+        secret:"Hello!",
+        resave: true,
+        saveUninitalized: true,
+}));
+
+app.use((req,res,next) => {
+    req.sessionStore.all((error, sessions) => {
+        console.log(sessions);
+        next();
+    });
+});
+
+app.get("/add-one",(req,res,next)=>{
+    return res.send(`${req.session.id}`);
+});
+
 app.use("/",rootRouter);
 app.use("/videos",videoRouter);
 app.use("/users",userRouter);
