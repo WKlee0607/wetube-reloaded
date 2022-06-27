0d099e0eede56a3f94f0e812bc7c7e996375d084
WKlee0607 <fovert@khu.ac.kr>
2022-06-27 10:54:31 +0900

    session에 정보 넣기

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -43,7 +43,8 @@ export const postLogin = async(req,res) => {
     if(!ok){
         return res.status(400).render("login",{pageTitle, errorMessage:"Wrong password"});
     }
-    console.log("LOG USER IN! COMING SOON");
+    req.session.loggedIn = true;
+    req.session.user = user;//우변의 user = await User.findOne({username}) -> 세션에 정보 담기.
     return res.redirect("/");
 }
 
