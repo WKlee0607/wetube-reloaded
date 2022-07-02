fa538230cbf3ca59c110fda72698863b0ce4f140
WKlee0607 <fovert@khu.ac.kr>
2022-07-03 00:25:28 +0900

    로그인/비로그인 유저가 방문할 수 있는 router를 나누기 위해 미드웨어 추가

--- src/middlewares.js
+++ src/middlewares.js
@@ -1,8 +1,24 @@
 export const localsMiddleware = (req, res, next) => {
     //console.log(req.session);
     res.locals.loggedIn = Boolean(req.session.loggedIn);//local안에 session의 loggedIn을 넣어줌
-    res.locals.loggedInUser = req.session.user;
+    res.locals.loggedInUser = req.session.user || {};
     res.locals.siteName = "Wetube";
     //console.log(res.locals)
     next();
+}
+
+export const protectorMiddleware = (req, res, next) => {
+    if(req.session.loggedIn){
+        return next();
+    } else{
+        return res.redirect("/login");
+    }
+};
+
+export const publicOnlyMiddleware = (req, res, next) => {
+    if(!req.session.loggedIn){
+        return next();
+    } else{
+        return res.redirect("/");
+    }
 }
