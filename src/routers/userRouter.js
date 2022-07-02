805342bf330bdad9ecb9f25e35aa9a4a23b18127
WKlee0607 <fovert@khu.ac.kr>
2022-07-03 02:33:16 +0900

    Change Password

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,14 +1,15 @@
 import express from "express";
-import {getEdit, postEdit, logout, see, startGithubLogin, callbackGithubLogin} from "../controllers/userController";
+import {getEdit, postEdit, logout, see, startGithubLogin, callbackGithubLogin, getChangePassword, postChangePassword} from "../controllers/userController";
 import { protectorMiddleware, publicOnlyMiddleware } from "../middlewares";
 
 const userRouter = express.Router();
 
 userRouter.get("/logout", protectorMiddleware, logout);
 userRouter.route("/edit").all(protectorMiddleware).get(getEdit).post(postEdit);
+userRouter.route("/change-password").all(protectorMiddleware).get(getChangePassword).post(postChangePassword);
 userRouter.get("/github/start", publicOnlyMiddleware, startGithubLogin);
 userRouter.get("/github/callback", publicOnlyMiddleware, callbackGithubLogin);
-userRouter.get("/:id",see);
+userRouter.get("/:id([0-9a-f]{24})",see);
 
 
 export default userRouter;
