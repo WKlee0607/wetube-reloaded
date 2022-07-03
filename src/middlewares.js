42c238921c1ac7cf6564e4b9d0542dde47ad5b09
WKlee0607 <fovert@khu.ac.kr>
2022-07-04 06:13:38 +0900

    multer
    multer를 이용해 avatar, video 파일 올릴 수 있도록 도와주는 middleware설정

--- src/middlewares.js
+++ src/middlewares.js
@@ -1,3 +1,5 @@
+import multer from "multer";
+
 export const localsMiddleware = (req, res, next) => {
     //console.log(req.session);
     res.locals.loggedIn = Boolean(req.session.loggedIn);//local안에 session의 loggedIn을 넣어줌
@@ -22,3 +24,11 @@ export const publicOnlyMiddleware = (req, res, next) => {
         return res.redirect("/");
     }
 }
+
+export const avatarUpload = multer({ dest: "uploads/avatars/", limits:{//limits: Limits of the uploaded data
+    fileSize: 3000000,//(단위: byte)
+}, });
+
+export const videoUpload = multer({ dest: "uploads/videos/", limits:{
+    fileSize: 10000000,
+}, });
