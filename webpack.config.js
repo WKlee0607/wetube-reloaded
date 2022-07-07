a7b59cb23a1693df916e951876b2ccefdbca0e05
WKlee0607 <fovert@khu.ac.kr>
2022-07-08 05:23:52 +0900

    watch , clean 추가
    1. watch: 수정하고 저장하면 그 수정사항을 바로 반영해줌 -> output으로 나옴
    2. clean을 하면 output을 build할 때 말 그대로 clean해줌

--- webpack.config.js
+++ webpack.config.js
@@ -4,15 +4,17 @@ const path = require("path");
 module.exports = {
     entry: "./src/client/js/main.js",
     mode: "development",
+    watch: true,//수정 사항이 있고 저장할 때마다 이를 반영해줌. -> 이 때문에 콘솔창을 2개 돌려야함.dev:server와 dev:assets. 이 둘은 동시에 실행되어야함.
     plugins: [new MiniCssExtractPlugin({
         filename:"css/styles.css"
     })],
     output: {
         filename:"js/main.js",
         path: path.resolve(__dirname, "assets"),
+        clean: true,
     },
     module :{
-        rules:[
+        rules:[//entry파일 내에 js파일 혹은 css파일 등을 어떤 loader로 처리할 지 지정해주는 곳
             {
                 test: /\.js$/,
                 use: {
