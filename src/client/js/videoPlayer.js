7865fcb67e752bdc9a802a905d586da6b140f165
WKlee0607 <fovert@khu.ac.kr>
2022-07-11 00:26:42 +0900

    time format & timeline추가

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -4,6 +4,10 @@ const muteBtn = document.getElementById("mute");
 const volumeRange = document.getElementById("volume");
 const currenTime = document.getElementById("currenTime");
 const totalTime = document.getElementById("totalTime");
+const timeline = document.getElementById("timeline");
+
+let videoPlayStatus = false;
+let setVideoPlayStatus = false;
 
 let changeVolume = 0.5;
 let volumeValue = 0.5;
@@ -27,6 +31,8 @@ const handleMute = (e) => {
     }
     muteBtn.innerText = video.muted ? "Unmute" : "Mute";
     volumeRange.value = video.muted ? 0 : volumeValue;
+    changeVolume = volumeValue;
+    video.volume = volumeValue;
 };
 
 const handleVolumeInput = (event) => {
@@ -49,13 +55,37 @@ const handleVolumeChange = (event) => {
     changeVolume = value;
 };
 
+const formatTime = (seconds) => {
+    if(seconds < 3600){
+        return new Date(seconds * 1000).toISOString().substring(14,19);
+    } 
+    return new Date(seconds * 1000).toISOString().substring(11,19);
+}
+
 const handleLoadedMetadata = () => {
-    totalTime.innerText = Math.floor(video.duration);
+    totalTime.innerText = formatTime(Math.floor(video.duration));
+    timeline.max = Math.floor(video.duration);
 };
 
 const handleTimeUpdate = () => {
-    currenTime.innerText = Math.floor(video.currentTime);
+    currenTime.innerText = formatTime(Math.floor(video.currentTime));
+    timeline.value = Math.floor(video.currentTime);// 비디오의 현재 진행시간을 타임라인에 나타내줌.
 };
+
+const handleTimelineChange = (event) => {
+    const {target: { value },} = event;
+    if (!setVideoPlayStatus) {
+    videoPlayStatus = video.paused ? false : true;
+    setVideoPlayStatus = true;
+    }
+    video.pause();
+    video.currentTime = value;
+    };
+
+const handleTimelineSet = () => {
+    videoPlayStatus ? video.play() : video.pause();
+    setVideoPlayStatus = false;
+    };
 
 playBtn.addEventListener("click", handlePlayClick);
 muteBtn.addEventListener("click", handleMute);
@@ -63,4 +93,6 @@ volumeRange.addEventListener("input", handleVolumeInput);
 volumeRange.addEventListener("change", handleVolumeChange);
 video.addEventListener("loadedmetadata", handleLoadedMetadata);
 video.addEventListener("timeupdate", handleTimeUpdate);
+timeline.addEventListener("input", handleTimelineChange);
+timeline.addEventListener("change", handleTimelineSet);
 
