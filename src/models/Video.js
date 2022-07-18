e874e6e2574db4e1bcb0a07a84d2c13f538a589f
WKlee0607 <fovert@khu.ac.kr>
2022-07-19 06:58:30 +0900

    Thumnail Schema추가

--- src/models/Video.js
+++ src/models/Video.js
@@ -3,6 +3,7 @@ import mongoose, { mongo, Mongoose } from "mongoose";
 const videoSchema = new mongoose.Schema({
     title: {type:String, required:true, trim:true, maxlength:80},//{type:String}이라 써도됨.
     fileUrl: {type:String, required: true},
+    thumbUrl: {type:String, required: true},
     description: {type:String, required:true, trim:true, minlength:10},
     createdAt: {type:Date, required:true, default:Date.now },
     hashtags:[{type:String, trim:true}],
