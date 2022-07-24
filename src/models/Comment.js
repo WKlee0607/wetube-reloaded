906aa9c41a3b49c2dca270382ad64cede66fb8c5
WKlee0607 <fovert@khu.ac.kr>
2022-07-24 14:52:22 +0900

    Comment model 만들기
    안에 video, user를 넣어줘서 relationship을 만듦.

---
+++ src/models/Comment.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+
+const commentSchema = new mongoose.Schema({
+    text: {type:String, required:true },
+    owner: { type: mongoose.Schema.Types.ObjectId, required: true, ref:"User" }, // User와 mongoose relationship
+    video: { type: mongoose.Schema.Types.ObjectId, required: true, ref:"Video" }, // Video와 mongoose relationship
+    createdAt: {type:Date, required:true, default:Date.now },
+});
+
+const Comment = mongoose.model("Comment", commentSchema); //"Comment"는 다른 Schema 등에서 ref로 사용되는 곳과 일치하도록 써줘야함.
+
+export default Comment;
