96bc0b1466dc8a292074c9441e3db3c6a88f29fa
WKlee0607 <fovert@khu.ac.kr>
2022-07-26 22:33:00 +0900

    fetch를 이용하여 comment 값을 해당 url로 post함. = api

--- src/client/js/commentSection.js
+++ src/client/js/commentSection.js
@@ -1,14 +1,21 @@
 const videoContainer = document.getElementById("videoContainer");
 const form = document.getElementById("commentForm");
-const textarea = form.querySelector("textarea");
-const btn = form.querySelector("button");
+
 
 
 const handleSubmit = (event) => {
     event.preventDefault();
+    const textarea = form.querySelector("textarea");
     const text = textarea.value;
-    const video = videoContainer.dataset.videoid
-    
+    const videoId = videoContainer.dataset.videoid
+    fetch(`/api/videos/${videoId}/comment`, {
+        method: "POST",
+        body: {// req.body를 만드는 과정
+            text,
+        }
+    })
 };
 
-form.addEventListener("submit", handleSubmit);
+if(form){
+    form.addEventListener("submit", handleSubmit);
+} // form이 로그인 한 상태에서만 보이기 떄문임.
