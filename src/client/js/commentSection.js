18e9e31a337bbe237be43015f52bf6bbcfa402d1
WKlee0607 <fovert@khu.ac.kr>
2022-07-30 22:00:02 +0900

    frontend에서 comment 처리
    1. comment를 fetch를 통해 backend로 보내기. 이 때 json.stringify를 이용하여 obj형태의 data를 보내기 & content-type을 이용해 보내는 데이터가 json임을 알리기
    2. addComment 함수를 이용하여 업로드된 댓글을 실시간으로 화면에 보이도록 하기(다른 유저에게 실시간으로 보이진 않음)

--- src/client/js/commentSection.js
+++ src/client/js/commentSection.js
@@ -1,19 +1,43 @@
+const { async } = require("regenerator-runtime");
+
 const videoContainer = document.getElementById("videoContainer");
 const form = document.getElementById("commentForm");
 
+const addComment = (text) => {
+    const videoComments = document.querySelector(".video__comments ul");
+    const newComment = document.createElement("li");
+    newComment.className = "video__comment";
+    const icon = document.createElement("i")
+    icon.className = "fas fa-comment";
+    const span = document.createElement("span");
+    span.innerText = ` ${text}`;
+    newComment.appendChild(icon);
+    newComment.appendChild(span);
+    videoComments.prepend(newComment);
+};
 
-
-const handleSubmit = (event) => {
+const handleSubmit = async (event) => {
     event.preventDefault();
     const textarea = form.querySelector("textarea");
     const text = textarea.value;
     const videoId = videoContainer.dataset.videoid
-    fetch(`/api/videos/${videoId}/comment`, {
+    if(text === ""){
+        return; //사용자가 아무것도 입력하지 않으면 req를 보내지 않음.
+    }
+    const response =  await fetch(`/api/videos/${videoId}/comment`, {
         method: "POST",
-        body: {// req.body를 만드는 과정
-            text,
-        }
-    })
+        headers : {
+            "Content-Type" : "application/json",
+        },
+        body: JSON.stringify({
+            text,// text(value값)만 보내면 object형태가 아닌 string형식의 text만 보내는 것임.
+        })
+    });
+    const status = response.status;
+    if(status === 201){
+        addComment(text);
+    }
+    textarea.value ="";
 };
 
 if(form){
