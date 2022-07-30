74a502114c31f86c66e505e297227d8246049d4b
WKlee0607 <fovert@khu.ac.kr>
2022-07-30 21:56:17 +0900

    json형식의 데이터 이해하는 미드웨어
    frontend에서 backend로 보내준 json.stringify형식의 데이터를 이해하도록 만드는 미드웨어

--- src/server.js
+++ src/server.js
@@ -18,6 +18,7 @@ app.set("view engine","pug");
 app.set("views",process.cwd() + "/src/views");//process.cwd: 현재 작업중인 파일 위치.
 app.use(logger);
 app.use(express.urlencoded({extended:true}));//form의 내용을 js의 array object 형태로 바꿔줌-> req.body를 생성함!
+app.use(express.json());
 
 
 app.use(
