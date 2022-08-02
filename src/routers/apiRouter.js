9cf30f92df31768e1b0f1dd518ab612fa24c9fb7
WKlee0607 <fovert@khu.ac.kr>
2022-08-02 18:24:06 +0900

    fakecomment에 commentid넣기 위한 fetch getRouter 생성

--- src/routers/apiRouter.js
+++ src/routers/apiRouter.js
@@ -1,10 +1,10 @@
 import express from "express";
-import { registerView , createComment, removeComment } from "../controllers/videoController";
+import { registerView , createComment, removeComment, getComment } from "../controllers/videoController";
 
 const apiRouter = express.Router();
 
 apiRouter.post("/videos/:id([0-9a-f]{24})/view", registerView);// api/videos/:id/view
-apiRouter.post("/videos/:id([0-9a-f]{24})/comment", createComment); // comment api router추가
+apiRouter.route("/videos/:id([0-9a-f]{24})/comment").post(createComment).get(getComment); // comment api router추가
 apiRouter.post("/videos/:id([0-9a-f]{24})/commentRemove", removeComment);
 
 export default apiRouter;
