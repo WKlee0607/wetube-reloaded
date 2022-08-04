ed228092e199a35784f9a33a3cc51c399e7f82fc
WKlee0607 <fovert@khu.ac.kr>
2022-08-05 06:16:55 +0900

    edit comment Router

--- src/routers/apiRouter.js
+++ src/routers/apiRouter.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { registerView , createComment, removeComment } from "../controllers/videoController";
+import { registerView , createComment, removeComment , editComment } from "../controllers/videoController";
 
 const apiRouter = express.Router();
 
 apiRouter.post("/videos/:id([0-9a-f]{24})/view", registerView);// api/videos/:id/view
 apiRouter.post("/videos/:id([0-9a-f]{24})/comment", createComment); // comment api router추가
 apiRouter.delete("/comment/:id([0-9a-f]{24})/remove", removeComment);
+apiRouter.post("/comment/:id([0-9a-f]{24})/edit", editComment);
 
 export default apiRouter;
