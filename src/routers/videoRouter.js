bf430e8bd200302403e5756e695885ca47928f22
WKlee0607 <fovert@khu.ac.kr>
2022-08-10 00:17:22 +0900

    AWS delete avatar

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -1,11 +1,11 @@
 import express from "express";
 import {watch, getEdit, postEdit, getUpload, postUpload, deleteVideo} from "../controllers/videoController";
-import { protectorMiddleware, videoUpload } from "../middlewares";
+import { protectorMiddleware, videoUpload,s3DeleteAvatar } from "../middlewares";
 
 const videoRouter = express.Router();
 
 videoRouter.get("/:id([0-9a-f]{24})", watch);
-videoRouter.route("/:id([0-9a-f]{24})/edit").all(protectorMiddleware).get(getEdit).post(postEdit);
+videoRouter.route("/:id([0-9a-f]{24})/edit").all(protectorMiddleware).get(getEdit).post(s3DeleteAvatar, postEdit);
 videoRouter.route("/:id([0-9a-f]{24})/delete").all(protectorMiddleware).get(deleteVideo);
 videoRouter.route("/upload").all(protectorMiddleware).get(getUpload).post(videoUpload.fields([
     {name:"video", maxCount: 1},
